b4888fb55d14c17d177c132579f28df0f7e39212
Monica Wheeler <monica.c.wheeler@gmail.com>
2018-01-08 09:58:09 -0600

    Create math router

--- server/server.js
+++ server/server.js
@@ -3,15 +3,14 @@ console.log('server.js working');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const mathRouter = require('./routes/math.router');
+
 const app = express();
 
 const PORT = 5000;
 
-app.get('/', (req, res) => {
-    res.sendStatus(200);
-});
+app.use('/math', mathRouter);
 
 app.listen(PORT, () => {
-    console.log(`up and running port: ${PORT}`);
-    
+    console.log(`up and running port: ${PORT}`); 
 });
